ff5237a99c3a1dbb018800c44b501d0e9ab0b259
MillsUnited <kingbowser1019@gmail.com>
2025-04-21 00:34:43 -0400

    Initial Commit

--- src/main/java/com/mills/antiItemBurn/AntiBurnEvent.java
+++ src/main/java/com/mills/antiItemBurn/AntiBurnEvent.java
@@ -1,4 +1,23 @@
 package com.mills.antiItemBurn;
 
-public class AntiBurnEvent {
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+public class AntiBurnEvent implements Listener {
+
+    @EventHandler
+    public void onItemBurn(EntityDamageEvent e) {
+        Entity entity = e.getEntity();
+
+        if (entity instanceof Item) {
+            if (e.getCause() == EntityDamageEvent.DamageCause.LAVA || e.getCause()
+                    == EntityDamageEvent.DamageCause.FIRE_TICK || e.getCause()
+                    == EntityDamageEvent.DamageCause.FIRE) {
+                e.setCancelled(true);
+            }
+        }
+    }
 }
