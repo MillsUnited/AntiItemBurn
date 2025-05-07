5a7dc479d47c3d75e228851996899cb15aaaacf0
MillsUnited <kingbowser1019@gmail.com>
2025-05-06 23:07:20 -0400

    Added mobs to the anti burn

--- src/main/java/com/mills/antiItemBurn/AntiBurnEvent.java
+++ src/main/java/com/mills/antiItemBurn/AntiBurnEvent.java
@@ -2,6 +2,7 @@ package com.mills.antiItemBurn;
 
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
+import org.bukkit.entity.Mob;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
@@ -12,10 +13,10 @@ public class AntiBurnEvent implements Listener {
     public void onItemBurn(EntityDamageEvent e) {
         Entity entity = e.getEntity();
 
-        if (entity instanceof Item) {
+        if (entity instanceof Item || entity instanceof Mob) {
             if (e.getCause() == EntityDamageEvent.DamageCause.LAVA || e.getCause()
                     == EntityDamageEvent.DamageCause.FIRE_TICK || e.getCause()
-                    == EntityDamageEvent.DamageCause.FIRE) {
+                    == EntityDamageEvent.DamageCause.FIRE || e.getCause() == EntityDamageEvent.DamageCause.DROWNING) {
                 e.setCancelled(true);
             }
         }
